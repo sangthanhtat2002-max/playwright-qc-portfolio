6dd9f70ff8882591531ea78fb04c1ac7ece27ce9
sangthanhtat2002-max <sangthanhtat2002@gmail.com>
2026-05-13 09:35:08 +0700

    test: add cart test cases

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -54,3 +54,37 @@ test.describe('Navigation', () => {
   });
 
 });
+
+test.describe('Cart', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.saucedemo.com');
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+  });
+
+  test('should add item to cart', async ({ page }) => {
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
+  });
+
+  test('should add multiple items to cart', async ({ page }) => {
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+    await expect(page.locator('.shopping_cart_badge')).toHaveText('2');
+  });
+
+  test('should remove item from cart', async ({ page }) => {
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+    await expect(page.locator('.shopping_cart_badge')).not.toBeVisible();
+  });
+
+  test('should navigate to cart page', async ({ page }) => {
+    await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+    await page.locator('.shopping_cart_link').click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/cart.html');
+  });
+
+});
